e4457d15812cd72f955246c8ce150d91b5647934
Raymond von Mizener <vmizener@gmail.com>
2019-12-13 15:39:29 -0800

    Moved bot event handlers outside init

--- mechabaozi/mechabaozi.py
+++ mechabaozi/mechabaozi.py
@@ -24,17 +24,41 @@ class MechaBaozi:
             description=BOT_DESCRIPTION,
             command_prefix=COMMAND_PREFIX,
         )
+        self.bot = bot
         self.logger.info('Bot spawned.')
 
-        @bot.event
+        self.register_on_ready()
+        self.register_on_command_error()
+
+        self.logger.info('Loading extensions:')
+        for extension_import_path in STARTUP_EXTENSIONS:
+            self.bot.load_extension(extension_import_path)
+            self.logger.info(f'- {extension_import_path}')
+
+        self.logger.info(f'Reading client info @ {CLIENT_INFO_PATH}')
+        with open(CLIENT_INFO_PATH, 'r') as file_handle:
+            json_dict = json.load(file_handle)
+            self.logger.debug(json.dumps(json_dict))
+            self.client_id = json_dict['client_id']
+            self.token = json_dict['client_token']
+            self.logger.debug(f'Token set: {self.token}.')
+
+        self.logger.info('Bot initialized successfully.')
+
+    def run(self):
+        self.logger.info('Activating bot.')
+        self.bot.run(self.token, bot=True, reconnect=True)
+
+    def register_on_ready(self):
         async def on_ready():
-            self.logger.info(f'Logged in as: {bot.user.name}#{bot.user.id}')
+            self.logger.info(f'Logged in as: {self.bot.user.name}#{self.bot.user.id}')
             self.logger.info(f'Version: {discord.__version__}')
-            server_strings = '\n'.join([f'- {s.name}::{s.id}' for s in bot.guilds])
-            self.logger.info(f'Running on {len(bot.guilds)} servers:\n{server_strings}')
+            server_strings = '\n'.join([f'- {s.name}::{s.id}' for s in self.bot.guilds])
+            self.logger.info(f'Running on {len(self.bot.guilds)} servers:\n{server_strings}')
+        self.bot.event(on_ready)
         self.logger.info('Registered "on_ready" method.')
 
-        @bot.event
+    def register_on_command_error(self):
         async def on_command_error(ctx, exception):
             if isinstance(exception, commands.errors.MissingPermissions):
                 await ctx.send(
@@ -83,24 +107,5 @@ class MechaBaozi:
                     await error_message.edit(embed=error_embed)
                 finally:
                     await error_message.remove_reaction(COMMAND_DEBUG_REACTION, ctx.bot.user)
+        self.bot.event(on_command_error)
         self.logger.info('Registered "on_command_error" method.')
-
-        self.logger.info('Loading extensions:')
-        for extension_import_path in STARTUP_EXTENSIONS:
-            bot.load_extension(extension_import_path)
-            self.logger.info(f'- {extension_import_path}')
-
-        self.logger.info(f'Reading client info @ {CLIENT_INFO_PATH}')
-        with open(CLIENT_INFO_PATH, 'r') as file_handle:
-            json_dict = json.load(file_handle)
-            self.logger.debug(json.dumps(json_dict))
-            self.client_id = json_dict['client_id']
-            self.token = json_dict['client_token']
-            self.logger.debug(f'Token set: {self.token}.')
-
-        self.bot = bot
-        self.logger.info('Bot initialized successfully.')
-
-    def run(self):
-        self.logger.info('Activating bot.')
-        self.bot.run(self.token, bot=True, reconnect=True)
